61c5cda412b63c8178e33fb832e349bf6f079bce
Denys Sidorov <000scorpions000@gmail.com>
2018-05-06 23:37:14 +0300

    add ssr v3

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# universal-react
-Try to get react app with rr4, redux, saga, ssr and code spliting
+# Isomorphic/Universal React App using NodeJS, React Router V4 and Redux
+Sample isomorphic demo using Redux, NodeJS, React Router V4, and ReactJS.
+
+## Install
+
+1. Clone repository:
+`git clone https://github.com/supercycle91/isomorphic-demo.git`
+2. Install dependencies: 
+`npm install`
+3. Start application:
+`npm start`
+
+See full tutorial [here](https://codeburst.io/react-isomorphic-universal-app-w-nodejs-redux-react-router-v4-be80aa57dcaf)
--- src/client/index.js
+++ src/client/index.js
@@ -1,45 +1,14 @@
-import { render } from 'react-dom';
-import React, { Component } from 'react';
-import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
-import createPalette from 'material-ui/styles/palette';
+import React from 'react';
+import { hydrate } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../shared/app/redux/store';
+import App from '../shared/app/app.jsx';
 
-import App from '../shared/app';
-
-const createStyleManager = () => MuiThemeProvider.createDefaultContext({
-    theme: createMuiTheme({
-        palette: createPalette({
-            // primary: green,
-            // accent: red,
-            type: 'light',
-        }),
-    }),
-});
-
-class Main extends Component {
-    // Remove the server-side injected CSS.
-    componentDidMount() {
-        const jssStyles = document.getElementById('jss-server-side');
-        if (jssStyles && jssStyles.parentNode) {
-            jssStyles.parentNode.removeChild(jssStyles);
-        }
-    }
-
-    render() {
-        return (
-            <Router>
-                <App {...this.props} />
-            </Router>
-        );
-    }
-}
-
-// Create a styleManager instance.
-const { styleManager, theme } = createStyleManager();
-
-render(
-    <MuiThemeProvider styleManager={styleManager} theme={theme}>
-        <Main />
-    </MuiThemeProvider>,
-    document.getElementById('root'),
-);
+hydrate((
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>
+), document.getElementById('root'));
